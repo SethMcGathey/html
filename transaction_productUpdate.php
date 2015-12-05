0c7c7c6efd214de73efbfee5b95f8cc5c6c607a4
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-05 11:12:27 -0600

    pushing staging to AWS through Github

--- transaction_productUpdate.php
+++ transaction_productUpdate.php
@@ -1,3 +1,69 @@
+<?php
+    require 'database.php';
+
+    $id = null;
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+
+    if ( null==$id ) {
+        header("Location: transaction_productIndex.php");
+    }
+
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $quantityError = null;
+        $transaction_idError = null;
+        $product_idError = null;
+
+        // keep track post values
+        $quantity = $_POST['quantity'];
+        $transaction_id = $_POST['transaction_id'];
+        $product_id = $_POST['product_id'];
+
+        // validate input
+        $valid = true;
+        if (empty($quantity)) {
+            $quantityError = 'Please enter Quantity';
+            $valid = false;
+        }
+
+        if (empty($transaction_id)) {
+            $transaction_idError = 'Please enter Quantity';
+            $valid = false;
+        }
+
+        if (empty($product_id)) {
+            $product_idError = 'Please enter Product Id';
+            $valid = false;
+        }
+
+
+
+        // update data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "UPDATE transaction_product  set quantity = ?, transaction_id = ?, product_id = ? WHERE id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($quantity,$transaction_id,$product_id,$id));
+            Database::disconnect();
+            header("Location: transaction_productIndex.php");
+        }
+    } else {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM transaction_product where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        $quantity = $data['quantity'];
+        $transaction_id = $data['transaction_id'];
+        $product_id = $data['product_id'];
+        Database::disconnect();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,47 +74,48 @@
 
 <body>
     <div class="container">
-            <div class="row">
-                <h3>PHP CRUD Grid</h3>
-            </div>
-            <div class="row">
-                <p>
-                    <a href="transaction_productCreate.php" class="btn btn-success">Create</a>
-                </p>
 
-                <table class="table table-striped table-bordered">
-                      <thead>
-                        <tr>
-                          <th>Quantity</th>
-                          <th>Transaction Id</th>
-                          <th>Product Id</th>
-                          <th>Action</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      <?php
-                       include 'database.php';
-                       $pdo = Database::connect();
-                       $sql = 'SELECT * FROM transaction_product ORDER BY id DESC';
-                       foreach ($pdo->query($sql) as $row) {
-                                echo '<tr>';
-                                echo '<td>'. $row['quantity'] . '</td>';
-                                echo '<td>'. $row['transaction_id'] . '</td>';
-                                echo '<td>'. $row['product_id'] . '</td>';
-                                echo '<td width=250>';
-                                echo '<a class="btn" href="transaction_productRead.php?id='.$row['id'].'">Read</a>';
-                                echo ' ';
-                                echo '<a class="btn btn-success" href="transaction_productUpdate.php?id='.$row['id'].'">Update</a>';
-                                echo ' ';
-                                echo '<a class="btn btn-danger" href="transaction_productDelete.php?id='.$row['id'].'">Delete</a>';
-                                echo '</td>';
-                                echo '</tr>';
-                       }
-                       Database::disconnect();
-                      ?>
-                      </tbody>
-                </table>
-        </div>
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Update a Transaction Product</h3>
+                    </div>
+
+                    <form class="form-horizontal" action="transaction_productUpdate.php?id=<?php echo $id?>" method="post">
+                      <div class="control-group <?php echo !empty($quantityError)?'error':'';?>">
+                        <label class="control-label">Quantity</label>
+                        <div class="controls">
+                            <input name="quantity" type="text"  placeholder="Quantity" value="<?php echo !empty($quantity)?$quantity:'';?>">
+                            <?php if (!empty($quantityError)): ?>
+                                <span class="help-inline"><?php echo $quantityError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($transaction_idError)?'error':'';?>">
+                        <label class="control-label">Transaction Id</label>
+                        <div class="controls">
+                            <input name="transaction_id" type="text"  placeholder="Transaction Id" value="<?php echo !empty($transaction_id)?$transaction_id:'';?>">
+                            <?php if (!empty($transaction_idError)): ?>
+                                <span class="help-inline"><?php echo $transaction_idError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($product_idError)?'error':'';?>">
+                        <label class="control-label">Product Id</label>
+                        <div class="controls">
+                            <input name="product_id" type="text" placeholder="Product Id" value="<?php echo !empty($product_id)?$product_id:'';?>">
+                            <?php if (!empty($product_idError)): ?>
+                                <span class="help-inline"><?php echo $product_idError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Update</button>
+                          <a class="btn" href="transaction_productIndex.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+
     </div> <!-- /container -->
   </body>
 </html>
