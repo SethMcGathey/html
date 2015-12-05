df2393af5c633543a832f9f2c47ac7346cad8198
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-05 09:28:38 -0600

    pushing staging to AWS through Github

---
+++ productCreate.php
@@ -0,0 +1,117 @@
+<?php
+
+    require 'database.php';
+
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $nameError = null;
+        $costError = null;
+        $descError = null;
+        $subcategory_idError = null;
+
+        // keep track post values
+        $name = $_POST['name'];
+        $cost = $_POST['cost'];
+        $desc = $_POST['desc'];
+        $subcategory_id = $_POST['subcategory_id'];
+
+        // validate input
+        $valid = true;
+        if (empty($name)) {
+            $nameError = 'Please enter Name';
+            $valid = false;
+        }
+
+        if (empty($cost)) {
+            $costError = 'Please enter Cost';
+            $valid = false;
+        }
+
+        if (empty($desc)) {
+            $descError = 'Please enter Description';
+            $valid = false;
+        }
+
+        if (empty($subcategory_id)) {
+            $subcategory_idError = 'Please enter Subcategory Id';
+            $valid = false;
+        }
+
+
+        // insert data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO product (name,cost,desc,subcategory_id) values(?, ?, ?, ?)";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($name, $cost, $desc,$subcategory_id));
+            Database::disconnect();
+            header("Location: productIndex.php");
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"></script>
+</head>
+
+<body>
+    <div class="container">
+
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Create a Product</h3>
+                    </div>
+
+                    <form class="form-horizontal" action="productCreate.php" method="post">
+                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <input name="name" type="text"  placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
+                            <?php if (!empty($nameError)): ?>
+                                <span class="help-inline"><?php echo $nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($costError)?'error':'';?>">
+                        <label class="control-label">Cost</label>
+                        <div class="controls">
+                            <input name="cost" type="text"  placeholder="Cost" value="<?php echo !empty($cost)?$cost:'';?>">
+                            <?php if (!empty($costError)): ?>
+                                <span class="help-inline"><?php echo $costError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($descError)?'error':'';?>">
+                        <label class="control-label">Description</label>
+                        <div class="controls">
+                            <input name="desc" type="text" placeholder="Description" value="<?php echo !empty($desc)?$desc:'';?>">
+                            <?php if (!empty($descError)): ?>
+                                <span class="help-inline"><?php echo $descError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($subcategory_idError)?'error':'';?>">
+                        <label class="control-label">Subcategory Id</label>
+                        <div class="controls">
+                            <input name="subcategory_id" type="text"  placeholder="Subcategory Id" value="<?php echo !empty($subcategory_id)?$subcategory_id:'';?>">
+                            <?php if (!empty($subcategory_idError)): ?>
+                                <span class="help-inline"><?php echo $subcategory_idError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Create</button>
+                          <a class="btn" href="productIndex.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+
+    </div> <!-- /container -->
+  </body>
+</html>
---
+++ productUpdate.php
@@ -0,0 +1,137 @@
+<?php
+    require 'database.php';
+
+    $id = null;
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+
+    if ( null==$id ) {
+        header("Location: productIndex.php");
+    }
+
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $nameError = null;
+        $costError = null;
+        $descError = null;
+        $subcategory_idError = null;
+
+        // keep track post values
+        $name = $_POST['name'];
+        $cost = $_POST['cost'];
+        $desc = $_POST['desc'];
+        $subcategory_id = $_POST['subcategory_id'];
+
+        // validate input
+        $valid = true;
+        if (empty($name)) {
+            $nameError = 'Please enter Name';
+            $valid = false;
+        }
+
+        if (empty($cost)) {
+            $costError = 'Please enter Name';
+            $valid = false;
+        }
+
+        if (empty($desc)) {
+            $descError = 'Please enter Description';
+            $valid = false;
+        }
+
+        if (empty($subcategory_id)) {
+            $subcategory_idError = 'Please enter Subcategory Id';
+            $valid = false;
+        }
+
+
+        // update data
+        if ($valid) {
+            $pdo = Database::connect();
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "UPDATE product  set name = ?, cost = ?, desc = ?, subcategory_id =? WHERE id = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($name,$cost,$desc,$subcategory_id,$id));
+            Database::disconnect();
+            header("Location: productIndex.php");
+        }
+    } else {
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT * FROM product where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        $name = $data['name'];
+        $cost = $data['cost'];
+        $desc = $data['desc'];
+        $subcategory_id = $data['subcategory_id'];
+        Database::disconnect();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Update a Product</h3>
+                    </div>
+
+                    <form class="form-horizontal" action="productUpdate.php?id=<?php echo $id?>" method="post">
+                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <input name="name" type="text"  placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
+                            <?php if (!empty($nameError)): ?>
+                                <span class="help-inline"><?php echo $nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($costError)?'error':'';?>">
+                        <label class="control-label">Cost</label>
+                        <div class="controls">
+                            <input name="cost" type="text"  placeholder="Cost" value="<?php echo !empty($cost)?$cost:'';?>">
+                            <?php if (!empty($costError)): ?>
+                                <span class="help-inline"><?php echo $costError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($descError)?'error':'';?>">
+                        <label class="control-label">Description</label>
+                        <div class="controls">
+                            <input name="desc" type="text" placeholder="Description" value="<?php echo !empty($desc)?$desc:'';?>">
+                            <?php if (!empty($descError)): ?>
+                                <span class="help-inline"><?php echo $descError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($subcategory_idError)?'error':'';?>">
+                        <label class="control-label">Subcategory Id</label>
+                        <div class="controls">
+                            <input name="subcategory_id" type="text"  placeholder="Subcategory Id" value="<?php echo !empty($subcategory_id)?$subcategory_id:'';?>">
+                            <?php if (!empty($subcategory_idError)): ?>
+                                <span class="help-inline"><?php echo $subcategory_idError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Update</button>
+                          <a class="btn" href="productIndex.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+
+    </div> <!-- /container -->
+  </body>
+</html>
