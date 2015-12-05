cb4df2525bfbd155cb0b12fb58e40c5a451bddbe
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-04 20:12:17 -0600

    pushing staging to AWS through Github

--- update.php
+++ update.php
@@ -12,8 +12,8 @@
 
     if ( !empty($_POST)) {
         // keep track validation errors
-        $firstnameError = null;
-        $lastnameError = null;
+        $first_nameError = null;
+        $last_nameError = null;
         $emailError = null;
         $phoneError = null;
         $dobError = null;
@@ -23,8 +23,8 @@
         $usernameError = null;
 
         // keep track post values
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
         $email = $_POST['email'];
         $phone = $_POST['phone'];
         $dob = $_POST['dob'];
@@ -35,13 +35,13 @@
 
         // validate input
         $valid = true;
-        if (empty($firstname)) {
-            $firstnameError = 'Please enter First Name';
+        if (empty($first_name)) {
+            $first_nameError = 'Please enter First Name';
             $valid = false;
         }
 
-        if (empty($lastname)) {
-            $lastnameError = 'Please enter First Name';
+        if (empty($last_name)) {
+            $last_nameError = 'Please enter First Name';
             $valid = false;
         }
 
@@ -88,21 +88,21 @@
         if ($valid) {
             $pdo = Database::connect();
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "UPDATE customer  set firstname = ?, lastname = ?, email = ?, phone =?, dob =?, gender =?, password =?, permission =?, username =? WHERE id = ?";
+            $sql = "UPDATE customer  set first_name = ?, last_name = ?, email = ?, phone =?, dob =?, gender =?, password =?, permission =?, username =? WHERE id = ?";
             $q = $pdo->prepare($sql);
-            $q->execute(array($firstname,$lastname,$email,$phone,$dob,$gender,$password,$permission,$username,$id));
+            $q->execute(array($first_name,$last_name,$email,$phone,$dob,$gender,$password,$permission,$username,$id));
             Database::disconnect();
             header("Location: index.php");
         }
     } else {
-        /*$pdo = Database::connect();
+        $pdo = Database::connect();
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = "SELECT * FROM customer where id = ?";
         $q = $pdo->prepare($sql);
         $q->execute(array($id));
         $data = $q->fetch(PDO::FETCH_ASSOC);
-        $firstname = $data['firstname'];
-        $lastname = $data['lastname'];
+        $first_name = $data['first_name'];
+        $last_name = $data['last_name'];
         $phone = $data['phone'];
         $email = $data['email'];
         $dob = $data['dob'];
@@ -110,7 +110,7 @@
         $password = $data['password'];
         $permission = $data['permission'];
         $username = $data['username'];
-        Database::disconnect();*/
+        Database::disconnect();
     }
 ?>
 
@@ -131,21 +131,21 @@
                     </div>
 
                     <form class="form-horizontal" action="update.php?id=<?php echo $id?>" method="post">
-                      <div class="control-group <?php echo !empty($firstnameError)?'error':'';?>">
+                      <div class="control-group <?php echo !empty($first_nameError)?'error':'';?>">
                         <label class="control-label">First Name</label>
                         <div class="controls">
-                            <input name="firstname" type="text"  placeholder="First Name" value="<?php echo !empty($firstname)?$firstname:'';?>">
-                            <?php if (!empty($firstnameError)): ?>
-                                <span class="help-inline"><?php echo $firstnameError;?></span>
+                            <input name="first_name" type="text"  placeholder="First Name" value="<?php echo !empty($first_name)?$first_name:'';?>">
+                            <?php if (!empty($first_nameError)): ?>
+                                <span class="help-inline"><?php echo $first_nameError;?></span>
                             <?php endif; ?>
                         </div>
                       </div>
-                      <div class="control-group <?php echo !empty($lastnameError)?'error':'';?>">
+                      <div class="control-group <?php echo !empty($last_nameError)?'error':'';?>">
                         <label class="control-label">Last Name</label>
                         <div class="controls">
-                            <input name="lastname" type="text"  placeholder="Last Name" value="<?php echo !empty($lastname)?$lastname:'';?>">
-                            <?php if (!empty($lastnameError)): ?>
-                                <span class="help-inline"><?php echo $lastnameError;?></span>
+                            <input name="last_name" type="text"  placeholder="Last Name" value="<?php echo !empty($last_name)?$last_name:'';?>">
+                            <?php if (!empty($last_nameError)): ?>
+                                <span class="help-inline"><?php echo $last_nameError;?></span>
                             <?php endif; ?>
                         </div>
                       </div>
