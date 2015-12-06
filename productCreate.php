de4fe252459ef86e29f9ba04285ccc4151784052
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-05 19:47:36 -0600

    pushing staging to AWS through Github

--- productCreate.php
+++ productCreate.php
@@ -98,16 +98,18 @@
                       <div class="control-group <?php echo !empty($subcategory_idError)?'error':'';?>">
                         <label class="control-label">Subcategory Id</label>
                         <div class="controls">
-                            <input name="subcategory_id" type="text"  placeholder="Subcategory Id" value="<?php echo !empty($subcategory_id)?$subcategory_id:'';?>">
+                            <input name="subcategory_id" type="text"  placeholder="Subcategory Id" value="<?php 
+                            echo
                             
-                      <?php include 'database.php'; ?>
-                      <?php $pdo = Database::connect(); ?>
-                      <?php $sql = 'SELECT id, name FROM subcategory ORDER BY id desc'; ?>
-                      <?php foreach ($pdo->query($sql) as $row) { ?>
-                      <?php          echo '<select><option value=' . $row['id'] . '>' . $row['name'] . '</option></select>'; ?>
-                      <?php } ?>
-                      <?php Database::disconnect(); ?>
-                      
+                            
+                           include 'database.php';
+                           $pdo = Database::connect();
+                           $sql = 'SELECT id, name FROM subcategory ORDER BY id desc';
+                           foreach ($pdo->query($sql) as $row) {
+                                    echo '<select><option value=' . $row['id'] . '>' . $row['name'] . '</option></select>';
+                           }
+                           Database::disconnect();
+                      ?>">
 
                             <?php if (!empty($subcategory_idError)): ?>
                                 <span class="help-inline"><?php echo $subcategory_idError;?></span>
