e8cf50d7bd512fdc77f85bc8f3440d375f134b88
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-05 22:11:21 -0600

    pushing staging to AWS through Github

--- productIndex.php
+++ productIndex.php
@@ -50,41 +50,6 @@
                       ?>
                       </tbody>
                 </table>
-
-                <table class="table table-striped table-bordered">
-                      <thead>
-                        <tr>
-                          <th>Name</th>
-                          <th>Cost</th>
-                          <th>Description</th>
-                          <th>Subcategory Id</th>
-                          <th>Action</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      <?php
-                       include 'database.php';
-                       $pdo = Database::connect();
-                       $sql = 'SELECT * FROM product ORDER BY id desc';
-                       foreach ($pdo->query($sql) as $row) {
-                          echo '<tr>';
-                          echo '<td>'. $row['name'] . '</td>';
-                          echo '<td>'. $row['cost'] . '</td>';
-                          echo '<td>'. $row['description'] . '</td>';
-                          echo '<td>'. $row['subcategory_id'] . '</td>';
-                          echo '<td width=250>';
-                          echo '<a class="btn" href="productRead.php?id='.$row['id'].'">Read</a>';
-                          echo ' ';
-                          echo '<a class="btn btn-success" href="productUpdate.php?id='.$row['id'].'">Update</a>';
-                          echo ' ';
-                          echo '<a class="btn btn-danger" href="productDelete.php?id='.$row['id'].'">Delete</a>';
-                          echo '</td>';
-                          echo '</tr>';
-                       }
-                       Database::disconnect();
-                      ?>
-                      </tbody>
-                </table>
         </div>
     </div> <!-- /container -->
   </body>
