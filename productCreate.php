105fa05f70fd51849eefae52dd1f71bc5dc6281b
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-05 21:28:50 -0600

    pushing staging to AWS through Github

--- productCreate.php
+++ productCreate.php
@@ -108,7 +108,7 @@
 
 
 
-                          
+                          <div>
                             <?php 
                                include 'database.php';
                                $pdo = Database::connect();
@@ -122,7 +122,7 @@
                                Database::disconnect(); 
                             ?>
                           
-
+</div>
 
 
 
