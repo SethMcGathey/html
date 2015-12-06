9b758067ec2406fd9d906d28d7dc330ae2d88d89
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-05 19:58:38 -0600

    pushing staging to AWS through Github

--- productCreate.php
+++ productCreate.php
@@ -98,19 +98,7 @@
                       <div class="control-group <?php echo !empty($subcategory_idError)?'error':'';?>">
                         <label class="control-label">Subcategory Id</label>
                         <div class="controls">
-                            <input name="subcategory_id" type="text"  placeholder="Subcategory Id" value="<?php 
-                            echo
-                            
-                            
-                           include 'database.php';
-                           $pdo = Database::connect();
-                           $sql = 'SELECT id, name FROM subcategory ORDER BY id desc';
-                           foreach ($pdo->query($sql) as $row) {
-                                    echo '<select><option value=' . $row['id'] . '>' . $row['name'] . '</option></select>';
-                           }
-                           Database::disconnect();
-                      ?>">
-
+                            <input name="subcategory_id" type="text"  placeholder="Subcategory Id" value="<?php echo !empty($subcategory_id)?$subcategory_id:'';?>">
                             <?php if (!empty($subcategory_idError)): ?>
                                 <span class="help-inline"><?php echo $subcategory_idError;?></span>
                             <?php endif;?>
