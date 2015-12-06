fb7cd0e20a2b838d64057038d5cc45c96190810d
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-05 22:24:22 -0600

    pushing staging to AWS through Github

--- productCreate.php
+++ productCreate.php
@@ -64,7 +64,7 @@
 <head>
     <meta charset="utf-8">
     <link   href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 
 <body>
