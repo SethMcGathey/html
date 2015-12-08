bfedf89a86a285fbe56a4cd51e95cc6f01f7d324
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-08 17:58:44 -0600

    pushing staging to AWS through Github

--- header.php
+++ header.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
 <link rel="stylesheet" type="text/css" href="css/styles.css">
 
 <h1>header.php</h1>
-
-</html>
--- navigation.php
+++ navigation.php
@@ -1,40 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Clash Games LLC</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head>
-  <body>
-
-    <nav class="navbar navbar-inverse">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>                        
-          </button>
-          <a class="navbar-brand" href="#">Clash Games</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">Home</a></li>
-            <li><a href="products.php">Products</a></li>
-            <li><a href="addProducts.php">Add Products</a></li>
-            <li><a href="contact.php">Contact</a></li>
-            <li><a href="manageUsers.php">Manage Users</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="register.php"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
-            <li><a href="cart.php"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-      
-    <div class="container">
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Clash Games</a>
     </div>
-
-  </body>
-</html>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="products.php">Products</a></li>
+        <li><a href="addProducts.php">Add Products</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="manageUsers.php">Manage Users</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="register.php"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
+        <li><a href="cart.php"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
