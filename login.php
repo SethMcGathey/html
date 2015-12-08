961da19341f43f2758dff6e1a61223a8cad255e0
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-07 20:35:53 -0600

    pushing staging to AWS through Github

---
+++ login.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require 'navigation.php' ?>
+
+<h1>login.php</h1>
+
+</html>
---
+++ register.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require 'navigation.php' ?>
+
+<h1>register.php</h1>
+
+</html>
