a32e4f5a5b9882e1d080360686079a589b3a2694
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-07 21:16:05 -0600

    pushing staging to AWS through Github

--- footer.php
+++ footer.php
@@ -3,6 +3,6 @@
 <div class="panel-footer">Panel Footer</div>
 
 
-  <link rel="stylesheet" href="js/bootstrap.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
   <script src="js/bootstrap.js"></script>
   <script src="js/jquery.js"></script>
