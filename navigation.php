a618720f1e127392890e7cf3bc46304369e564c6
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-08 18:29:27 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -17,6 +17,7 @@
         <li><a href="manageUsers.php">Manage Users</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
+        <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
         <li><a href="register.php"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
         <li><a href="cart.php"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
       </ul>
