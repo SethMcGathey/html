ecb3d617c060dcb1cda2f1968ed58a1268906f28
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-08 18:55:54 -0600

    pushing staging to AWS through Github

--- index.php
+++ index.php
@@ -8,12 +8,10 @@
 		<div class="container-fluid">
 			<h1>Index.php</h1>
 			<div class="row">
-				<div class="col-4-lg">Games</div>
-				<div class="col-4-lg">Toys</div>
-				<div class="col-4-lg">Puzzles</div>
+				<div class="col-4-lg"><a href="">Games</a></div>
+				<div class="col-4-lg"><a href="">Toys</a></div>
+				<div class="col-4-lg"><a href="">Puzzles</a></div>
 			</div>
-
-		
 		</div>
 
 	</body>
