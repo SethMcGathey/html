dac67266f05805e221e8f19559b080272252c15e
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-08 19:38:44 -0600

    pushing staging to AWS through Github

--- index.php
+++ index.php
@@ -9,18 +9,24 @@
 			<h1>Index.php</h1>
 			<div class="row">
 
-
-				<?php
-		           require 'database.php';
-		           $pdo = Database::connect();
-		           $sql = 'SELECT id,name FROM category ORDER BY id DESC';
-		           foreach ($pdo->query($sql) as $row) {
-		           		echo '<a href=""><div class="col-4-lg" id='.$row['id'].'>'. $row['name'] . '</div></a>'
-		           }
-		           Database::disconnect();
-		        ?>
-
-
+              <?php
+               include '../../database.php';
+               $pdo = Database::connect();
+               $sql = 'SELECT * FROM category ORDER BY id DESC';
+               foreach ($pdo->query($sql) as $row) {
+                        echo '<tr>';
+                        echo '<td>'. $row['name'] . '</td>';
+                        echo '<td width=250>';
+                        echo '<a class="btn" href="categoryRead.php?id='.$row['id'].'">Read</a>';
+                        echo ' ';
+                        echo '<a class="btn btn-success" href="categoryUpdate.php?id='.$row['id'].'">Update</a>';
+                        echo ' ';
+                        echo '<a class="btn btn-danger" href="categoryDelete.php?id='.$row['id'].'">Delete</a>';
+                        echo '</td>';
+                        echo '</tr>';
+               }
+               Database::disconnect();
+              ?>
 
 				<a href=""><div class="col-4-lg">Games</div></a>
 				<a href=""><div class="col-4-lg">Toys</div></a>
