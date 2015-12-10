3fb2646eae4b9785d30239c042de913c0c3adcc4
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-09 19:09:19 -0600

    pushing staging to AWS through Github

--- selectSubcategory.php
+++ selectSubcategory.php
@@ -1,11 +1,12 @@
 <?php
 	include 'database.php';
+	echo "hi";
     $pdo = Database::connect();
 
 	$sql = 'SELECT id,name,category_id FROM subcategory WHERE category_id = ' id;
 	foreach ($pdo->query($sql) as $row) {
 	    echo '<a href=""><div class="col-4-lg" id="' . $row['id']. '">' . $row['name'] . '</div></a>';
 	}
-	
+
 	Database::disconnect();
 ?>
