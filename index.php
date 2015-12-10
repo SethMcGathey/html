45c8200c267f016cb018cde5ba8c7a597f912ed4
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-09 19:36:42 -0600

    pushing staging to AWS through Github

--- index.php
+++ index.php
@@ -55,7 +55,8 @@ $(document).ready(function(){
 		var clickedId = this.id;
 		console.log(clickedId);
 		$.get( "selectSubcategory.php?id=" + clickedId, function( data ) {
-  			document.getElementById("#ajaxOutput").HTML = data
+  			//document.getElementById("#ajaxOutput").HTML = data
+  			$( this ).text( data );
 		});
 	});
 });
