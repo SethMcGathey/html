0f01fa3daa624e4e962074eb8cb5f22fd47441cd
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-09 21:36:28 -0600

    pushing staging to AWS through Github

--- footer.php
+++ footer.php
@@ -8,8 +8,8 @@
 
 
 <link rel="stylesheet" href="css/bootstrap.min.css">
-<script src="js/javascript.js"></script>
 <script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/javascript.js"></script>
 <script src="js/bootstrap.min.js"></script>
 
 
