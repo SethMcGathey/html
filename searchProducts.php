b3ae3137fc06b003f4973190650993bb631e0bd5
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-12 13:37:09 -0600

    pushing staging to AWS through Github

--- searchProducts.php
+++ searchProducts.php
@@ -1,7 +1,7 @@
 <?php
 	include 'database.php';
     $pdo = Database::connect();
-	
+	echo 'made it to this one first';
 		$sql = 'SELECT id,name,cost,description FROM product WHERE name LIKE \'%' . $_GET["search"] . '%\' ORDER BY id LIMIT 5';
 		foreach ($pdo->query($sql) as $row) {
 		    echo '<div class="col-4-lg product" id="' . $row['id']. '">' . $row['name'] . ' ' . $row['description'] . ' ' . $row['cost'] . ' <a href="#">Add to Cart</a></div>';
