5a026237735044ecd8f9d3b6addd0d9dcf315742
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-11 19:48:19 -0600

    pushing staging to AWS through Github

--- products.php
+++ products.php
@@ -8,9 +8,18 @@
 
 
 			<?php
-				$sql = 'SELECT id,name,cost,description FROM product WHERE subcategory_id = ' . $_GET["id"] . ' ORDER BY id';
-				foreach ($pdo->query($sql) as $row) {
-				    echo '<a href="#"><div class="col-4-lg product" id="' . $row['id']. '">' . $row['name'] . ' ' . $row['description'] . ' ' . $row['cost'] . '</div></a>';
+				if(isset($_GET['id']))
+				{
+					$sql = 'SELECT id,name,cost,description FROM product WHERE subcategory_id = ' . $_GET["id"] . ' ORDER BY id';
+					foreach ($pdo->query($sql) as $row) {
+					    echo '<a href="#"><div class="col-4-lg product" id="' . $row['id']. '">' . $row['name'] . ' ' . $row['description'] . ' ' . $row['cost'] . '</div></a>';
+					}
+				}else
+				{
+					$sql = 'SELECT TOP 5 id,name,cost,description FROM product ORDER BY id';
+					foreach ($pdo->query($sql) as $row) {
+					    echo '<a href="#"><div class="col-4-lg product" id="' . $row['id']. '">' . $row['name'] . ' ' . $row['description'] . ' ' . $row['cost'] . '</div></a>';
+					}
 				}
 			?>
 
