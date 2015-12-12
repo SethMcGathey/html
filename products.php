0ab616ae1d3a210b6db480c97b3030ea4a1658c8
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-11 19:26:03 -0600

    pushing staging to AWS through Github

--- products.php
+++ products.php
@@ -8,10 +8,10 @@
 
 
 			<?php
-			$sql = 'SELECT id,name,cost,description FROM product WHERE subcategory_id = ' . $_GET["id"] . ' ORDER BY id';
-			foreach ($pdo->query($sql) as $row) {
-			    echo '<a href="#"><div class="col-4-lg product" id="' . $row['id']. '">' . $row['name'] . '</div></a>';
-			}
+				$sql = 'SELECT id,name,cost,description FROM product WHERE subcategory_id = ' . $_GET["id"] . ' ORDER BY id';
+				foreach ($pdo->query($sql) as $row) {
+				    echo '<a href="#"><div class="col-4-lg product" id="' . $row['id']. '">' . $row['name'] . . $row['description'] .. $row['cost'] .. $row['name'] .'</div></a>';
+				}
 			?>
 
 			<div class="row"><div class=""></div><div></div></div>
