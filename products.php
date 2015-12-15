3b9f7a25e5a2d89db5edb1a132d8db59ad2c3f68
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-14 21:53:31 -0600

    pushing staging to AWS through Github

--- products.php
+++ products.php
@@ -11,16 +11,16 @@
 				$pdo->setAttribute(PDO::ATTR_FETCH_TABLE_NAMES, true);
 				if(isset($_GET['id']))
 				{
-					$sql = 'SELECT a.id,a.name,a.cost,a.description,b.description, b.image FROM product a LEFT JOIN image b ON a.id = b.product_id WHERE a.subcategory_id = ' . $_GET["id"] . ' ORDER BY id LIMIT 5';
+					$sql = 'SELECT a.id,a.name,a.cost,a.description,b.description,b.image FROM product a LEFT JOIN image b ON a.id = b.product_id WHERE a.subcategory_id = ' . $_GET["id"] . ' ORDER BY id LIMIT 5';
 					//$sql = 'SELECT id,name,cost,description FROM product WHERE subcategory_id = ' . $_GET["id"] . ' ORDER BY id LIMIT 5';
 					foreach ($pdo->query($sql) as $row) {
 					    echo '<div class="col-4-lg product" id="' . $row['a.id']. '">' . $row['a.name'] . ' ' . $row['a.description'] . ' ' . $row['a.cost'] . ' <a href="#">Add to Cart</a></div>';
 					}
 				}else
 				{
-					$sql = 'SELECT a.id,a.name,a.cost,a.description,b.description, b.image FROM product a LEFT JOIN image b ON a.id = b.product_id ORDER BY a.id LIMIT 5';
+					$sql = 'SELECT a.id,a.name,a.cost,a.description,b.description,b.image FROM product a LEFT JOIN image b ON a.id = b.product_id ORDER BY a.id LIMIT 5';
 					foreach ($pdo->query($sql) as $row) {
-					    echo '<div class="col-4-lg product" id="' . $row['a.id'] . '">' . '<img src="data:image/jpeg;base64,' . base64_decode($row['b.image']) . '" ' . $row['a.name'] . ' ' . $row['a.description'] . ' ' . $row['a.cost'] . ' <a href="#">Add to Cart</a></div>';
+					    echo '<div class="col-4-lg product" id="' . $row['a.id'] . '">' . '<img src="data:image/jpeg;base64,' . base64_decode($row['b.image']) . '/>" ' . $row['a.name'] . ' ' . $row['a.description'] . ' ' . $row['a.cost'] . ' <a href="#">Add to Cart</a></div>';
 					}
 				}
 			?>
