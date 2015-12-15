a83f95012be7b54e18f965f7ca2156fbc427b5a3
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-14 18:25:32 -0600

    pushing staging to AWS through Github

--- login.php
+++ login.php
@@ -6,9 +6,9 @@
 		<div class="container" id="Not_Ajax_Output">
 			<h1>login.php</h1>
 
-			<p>User Name</p><input type="text" class="form-control" placeholder="User Name" name="usernameInput" id="usernameInput">
-			<p>Password</p><input type="text" class="form-control" placeholder="Password" name="passwordInput" id="passwordInput">
-			
+			<p>User Name:</p><input type="text" class="form-control" placeholder="User Name" name="usernameInput" id="usernameInput">
+			<p>Password:</p><input type="text" class="form-control" placeholder="Password" name="passwordInput" id="passwordInput">
+
 		</div>
 
 	</body>
