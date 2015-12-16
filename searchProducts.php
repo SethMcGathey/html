03f3133ba111102b0871ce8937be9a481d2d94c9
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-15 20:01:52 -0600

    pushing staging to AWS through Github

--- searchProducts.php
+++ searchProducts.php
@@ -2,7 +2,7 @@
 	include 'database.php';
     $pdo = Database::connect();
 
-		$sql = 'SELECT a.id,a.name,a.cost,a.description,b.image FROM product a LEFT JOIN image b ON a.id = b.product_id WHERE a.name LIKE \'%' . $_GET["id"] . '%\'OR a.description LIKE \'%' . $_GET["id"] . '%\' OR a.cost LIKE \'%' . $_GET["id"] . '%\' ORDER BY id LIMIT 5';
+		$sql = 'SELECT a.id,a.name,a.cost,a.description,b.image FROM product a LEFT JOIN image b ON a.id = b.product_id WHERE a.name LIKE \'%' . $_GET["id"] . '%\'OR a.description LIKE \'%' . $_GET["id"] . '%\' OR a.cost LIKE \'%' . $_GET["id"] . '%\' ORDER BY a.id LIMIT 5';
 		foreach ($pdo->query($sql) as $row) {
 		    echo '<div class="col-4-lg product" id="' . $row['a.id']. '">' . '<img src="data:image/jpeg;base64,' . base64_encode($row['b.image']) . '"width="100px"/> ' . $row['a.name'] . ' ' . $row['a.description'] . ' ' . $row['a.cost'] . ' <a href="#">Add to Cart</a></div>';
 		
