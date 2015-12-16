2c4d43f8c62abfc6e81bde955d385456c3fed989
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-15 20:45:37 -0600

    pushing staging to AWS through Github

--- searchProducts.php
+++ searchProducts.php
@@ -3,7 +3,7 @@
     $pdo = Database::connect();
 
 		//$sql = 'SELECT a.id,a.name,a.cost,a.description,b.image FROM product a LEFT JOIN image b ON a.id = b.product_id WHERE a.name LIKE \'%' . $_GET["id"] . '%\'OR a.description LIKE \'%' . $_GET["id"] . '%\' OR a.cost LIKE \'%' . $_GET["id"] . '%\' ORDER BY a.id LIMIT 5';
-
+    	$pdo->setAttribute(PDO::ATTR_FETCH_TABLE_NAMES, true);
 		$sql = 'SELECT a.id,a.name,a.cost,a.description,b.image FROM product a LEFT JOIN image b ON a.id = b.product_id WHERE a.name LIKE \'%' . $_GET["id"] . '%\' OR a.description LIKE \'%' . $_GET["id"] . '%\' OR a.cost LIKE \'%' . $_GET["id"] . '%\' ORDER BY a.id LIMIT 5';
 
 		foreach ($pdo->query($sql) as $row) {
