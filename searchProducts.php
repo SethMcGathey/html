022af61864a755a35679a18ca9c19a186523339f
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-15 19:48:49 -0600

    pushing staging to AWS through Github

--- searchProducts.php
+++ searchProducts.php
@@ -2,7 +2,7 @@
 	include 'database.php';
     $pdo = Database::connect();
 
-		$sql = 'SELECT id,name,cost,description FROM product WHERE name LIKE \'%' . $_GET["id"] . '%\'OR description LIKE \'%' . $_GET["id"] . '%\' OR cost LIKE \'%' . $_GET["id"] . '%\' ORDER BY id LIMIT 5';
+		$sql = 'SELECT a.id,a.name,a.cost,a.description,b.image FROM product a LEFT JOIN image b ON a.id = b.product_id WHERE name LIKE \'%' . $_GET["id"] . '%\'OR description LIKE \'%' . $_GET["id"] . '%\' OR cost LIKE \'%' . $_GET["id"] . '%\' ORDER BY id LIMIT 5';
 		foreach ($pdo->query($sql) as $row) {
 		    echo '<div class="col-4-lg product" id="' . $row['id']. '">' . '<img src="data:image/jpeg;base64,' . base64_encode($row['b.image']) . '"width="100px"/> ' . $row['name'] . ' ' . $row['description'] . ' ' . $row['cost'] . ' <a href="#">Add to Cart</a></div>';
 		
