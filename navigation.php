ed81181bc369f5f71a229e196b54ddf561a70893
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-22 19:42:59 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -20,7 +20,9 @@
         <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
         <li><a href="register.php"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
         <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
-        <?php if(isset($_SESSION['username']))
+        <?php 
+              echo $_SESSION['username'];
+              if(isset($_SESSION['username']))
               {
                 echo '<li><a href="login.php">' . $_SESSION['username'] . '</a></li>';
               }
