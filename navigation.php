cd70c84b175e9b4dae7aeeca0d5e17f52392de6d
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-23 17:29:01 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -20,11 +20,13 @@
         <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
         <li><a href="register.php"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
         
+
         <?php 
+              //changes login to logout when a person logs in and puts their name in the Navbar
               if(isset($_SESSION['username']))
               {
                 echo '<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>Logout</a></li>';
-                echo '<li><a href="login.php">' . $_SESSION['username'] . '</a></li>';
+                echo '<li><a href="login.php">Welcome, ' . $_SESSION['username'] . '</a></li>';
               }else
               {
                 echo '<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>';
