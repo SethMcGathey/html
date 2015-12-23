b2982993d779f0438533fa9c1528e638190893e2
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-22 20:36:13 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -21,6 +21,7 @@
         <li><a href="register.php"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
         <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>
         <?php 
+        echo 'poop';
               echo $_SESSION['username'];
               if(isset($_SESSION['username']))
               {
