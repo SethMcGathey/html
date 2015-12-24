52ffc2abbc231d231ca794d8e8f8f9cd49e751dd
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-24 11:08:29 -0600

    pushing staging to AWS through Github

--- register.php
+++ register.php
@@ -5,6 +5,18 @@
 		<?php require 'navigation.php' ?>
 
 		<h1>register.php</h1>
+
+		<div class="container" id="Not_Ajax_Output">
+			<h1>login.php</h1>
+			<form action="verify.php" method="POST">
+				<p>User Name:</p><input type="text" placeholder="User Name" name="usernameInput" id="usernameInput">
+				<p>Password:</p><input type="password" placeholder="Password" name="passwordInput" id="passwordInput">
+				<p>Reenter Password:</p><input type="password" placeholder="Password" name="passwordInput" id="passwordInput">
+				<button>Log In</button>
+			</form>
+
+		</div>
+
 	</body>
 
 	<?php require 'footer.php' ?>
