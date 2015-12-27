135f40e9c6493285d8537c1966391fc286129129
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-27 13:53:07 -0600

    pushing staging to AWS through Github

--- register.php
+++ register.php
@@ -7,15 +7,15 @@
 		<div class="container" id="Not_Ajax_Output">
 			<h1>Register</h1>
 			<form action="registerUser.php" method="POST">
-				<p>First Name:</p><input type="text" placeholder="First Name" name="firstNameInput" id="usernameInput">
-				<p>Last Name:</p><input type="text" placeholder="Last Name" name="lastNameInput" id="usernameInput">
-				<p>User Name:</p><input type="text" placeholder="User Name" name="userNameInput" id="usernameInput">
+				<p>First Name:</p><input type="text" placeholder="First Name" name="firstNameInput" id="firstNameInput">
+				<p>Last Name:</p><input type="text" placeholder="Last Name" name="lastNameInput" id="lastNameInput">
+				<p>User Name:</p><input type="text" placeholder="User Name" name="userNameInput" id="userNameInput">
 				<p>Password:</p><input type="password" placeholder="Password" name="passwordInput" id="passwordInput">
-				<p>Reenter Password:</p><input type="password" placeholder="Password" name="reenteredPasswordInput" id="passwordInput">
-				<p>Phone Number:</p><input type="text" placeholder="Phone Number" name="phoneNumberInput" id="usernameInput">
-				<p>Date of Birth:</p><input type="text" placeholder="Date of Birth" name="dobInput" id="usernameInput">
-				<p>Gender:</p><input type="text" placeholder="Gender" name="usernameInput" id="genderInput">
-				<p>Email:</p><input type="text" placeholder="Email" name="usernameInput" id="emailInput">
+				<p>Reenter Password:</p><input type="password" placeholder="Password" name="reenteredPasswordInput" id="reenteredPasswordInput">
+				<p>Phone Number:</p><input type="text" placeholder="Phone Number" name="phoneNumberInput" id="phoneNumberInput">
+				<p>Date of Birth:</p><input type="text" placeholder="Date of Birth" name="dobInput" id="dobInput">
+				<p>Gender:</p><input type="text" placeholder="Gender" name="genderInput" id="genderInput">
+				<p>Email:</p><input type="text" placeholder="Email" name="emailInput" id="emailInput">
 
 				<button>Log In</button>
 			</form>
--- registerUser.php
+++ registerUser.php
@@ -3,48 +3,37 @@
 	include 'database.php';
     $pdo = Database::connect();
 
-	echo $_SESSION['user'];
+	//echo $_SESSION['user'];
 	if($_SERVER["REQUEST_METHOD"] == "POST")
 
-	$myusername = $_POST['usernameInput'];
-	$mypassword = $_POST['passwordInput'];
-	$_SESSION['user'] = $myusername;
-
-	//$myusername=mysqli_real_escape_string($db, $_POST['username']);
-	//$mypassword=mysqli_real_escape_string($db, $_POST['password']);
-	echo "<div>" . $myusername . "</div>";
-	echo "<div>" . $mypassword . "</div>";
-	/*
-	$sql="SELECT id, first_name, password FROM customer WHERE username = " . $myusername;
-
-
-	foreach ($pdo->query($sql) as $row) {
-		echo "<div>garbage</div>";
-		echo '<div class="col-4-lg product" id="' . $row['id']. '">' . $row['first_name'] . ' ' . $row['password'] . '</div>';
+	$password = $_POST['passwordInput'];
+	$reenterPassword = $_POST['reenteredPasswordInput'];
+	if($password == $reenterPassword)
+	{
+		$firstName = $_POST['firstNameInput'];
+		$lastName = $_POST['lastNameInput'];
+		$username = $_POST['userNameInput'];
+		$phoneNumber = $_POST['phoneNumberInput'];
+		$dob = $_POST['dobInput'];
+		$gender = $_POST['genderInput'];
+		$email = $_POST['emailInput'];
+		$_SESSION['user'] = $myusername;
 	}
-	//echo 'first_name';
-	Database::disconnect();
-*/
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql="SELECT id, username, first_name, password, permission FROM customer WHERE username = ? AND password = ?";
-    $q = $pdo->prepare($sql);
-    $q->execute(array($myusername, $mypassword));
-    $data = $q->fetch(PDO::FETCH_ASSOC);
-    if(isset($data['id']))
-    {
-    	//echo $data['id'];
-    	//echo '<div class="col-4-lg product" id="' . $data['id'] . '">' . $data['first_name'] . ' ' . $data['password'] . '</div>';
-    	
+	if(isset($firstName) && isset($lastName) && isset($username) && isset($phoneNumber) && isset($dob) && isset($gender) && isset($email) && isset($password))
+	{
+	    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql="INSERT INTO customer (first_name, phone, dob, username, password, gender, permission, email, last_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	    $q = $pdo->prepare($sql);
+	    $q->execute(array($firstName, $phoneNumber, $dob, $username, $password, $gender, 1, $email, $lastName));
+	    $data = $q->fetch(PDO::FETCH_ASSOC);
+
     	$_SESSION['username'] = $data['username'];
     	$_SESSION['customerid'] = $data['id'];
     	$_SESSION['permission'] = $data['permission'];
     	echo $_SESSION['permission'];
     	header('Location: index.php');
-    }else
-    {
-    	echo "Invalid Login";
-    }
+	}
 print_r($_SESSION);
  	Database::disconnect();
 
