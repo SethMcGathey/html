6568a6815fd31a9a8805749ec3475f0a6dbed1f1
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-28 18:18:15 -0600

    pushing staging to AWS through Github

--- header.php
+++ header.php
@@ -1,6 +1,10 @@
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css/styles.css">
-
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Ecommerce</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
 
 <?php
 	include 'database.php';
