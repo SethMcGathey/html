64a7d47fe93fcc0170a1703343c2cd5bcbeff06a
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-28 19:09:20 -0600

    pushing staging to AWS through Github

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
               <?php
                $sql = 'SELECT id,name FROM category ORDER BY id';
                foreach ($pdo->query($sql) as $row) {
-	                echo '<a href="#"><img src="img/rrwggame.jpg" width="100px"/><div class="col-lg-4 myCategories" id="' . $row['id']. '">' . $row['name'] . '</div></a>';
+	                echo '<a href="#"><div class="col-lg-4 myCategories" id="' . $row['id']. '"><img src="img/rrwggame.jpg" width="100px"/>' . $row['name'] . '</div></a>';
                }
               ?>
           	</div>
