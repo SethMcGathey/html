87b807f4ed4fe2dd50d831e7fd03ece44c2ef407
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-30 21:15:19 -0600

    pushing staging to AWS through Github

--- profile.php
+++ profile.php
@@ -19,19 +19,20 @@
 				</div>
 				<div class="col-lg-3">
 					<h3>Current Address</h3>
+					<div class="scrollbox">
 					<?php
 						//echo $_SESSION['customerid'];
 		               	$sql = "SELECT street_one, street_two, zipcode, city, state, country FROM customer_address c JOIN address a ON c.address_id = a.id WHERE customer_id = " . $_SESSION['customerid'];
 		               	foreach ($pdo->query($sql) as $row) {
-			               	echo '<p>Street 1:</p><p name="street1" id="street1">' . $row['street_one'] . '</p>
-								  <p>Street 2:</p><p name="street2" id="street2">' . $row['street_two'] . '</p>
-								  <p>Zipcode:</p><p name="zipcode" id="zipcode">' . $row['zipcode'] . '</p>
-								  <p>City:</p><p name="city" id="city">' . $row['city'] . '</p>
-								  <p>State:</p><p name="state" id="state">' . $row['state'] . '</p>
-								  <p>Country:</p><p name="country" id="country">' . $row['country'] . '</p>';
+			               	echo '<p name="street1" id="street1">Street 1: ' . $row['street_one'] . '</p>
+								  <p name="street2" id="street2">Street 2: ' . $row['street_two'] . '</p>
+								  <p name="zipcode" id="zipcode">Zipcode: ' . $row['zipcode'] . '</p>
+								  <p name="city" id="city">City:' . $row['city'] . '</p>
+								  <p name="state" id="state">State:' . $row['state'] . '</p>
+								  <p name="country" id="country">Country:' . $row['country'] . '</p>';
 		               }
 		            ?>
-
+		        	</div>
 				</div>
 				<div class="col-lg-3">
 					<h3>New Card</h3>
