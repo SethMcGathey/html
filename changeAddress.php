98612a51d76f342fd86376ec4fb5e555de77b261
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-31 13:22:49 -0600

    pushing staging to AWS through Github

--- changeAddress.php
+++ changeAddress.php
@@ -10,10 +10,10 @@
 	if(trim($_POST['street1']) != "" && trim($_POST['zipcode']) != "" && trim($_POST['city']) != "" && trim($_POST['state']) != "" && trim($_POST['country']) != "")
 	{
 		echo 'made it inside if';
-	    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    /*$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$sql="INSERT INTO address (street_one, street_two, zipcode, city, state, country) VALUES (?, ?, ?, ?, ?, ?)";
 	    $q = $pdo->prepare($sql);
-	    $q->execute(array($_POST['street1'], $_POST['street2'], $_POST['zipcode'], $_POST['city'], $_POST['state'], $_POST['country']));
+	    $q->execute(array($_POST['street1'], $_POST['street2'], $_POST['zipcode'], $_POST['city'], $_POST['state'], $_POST['country']));*/
 
 	    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$sql="SELECT ID FROM address ORDER BY ID desc LIMIT 1";
@@ -26,10 +26,10 @@
 	    echo $pdo::db2_last_insert_id($sql);
 	    echo mysql_insert_id();
 	    //echo $_SESSION['customerid'];
-	   	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	   	/*$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$sql="INSERT INTO customer_address (customer_id, address_id) VALUES (?, ?)";
 	    $q = $pdo->prepare($sql);
-	    $q->execute(array($_SESSION['customerid'], 1));
+	    $q->execute(array($_SESSION['customerid'], 1));*/
 	    
     	//header('Location: profile.php');
 	}else
