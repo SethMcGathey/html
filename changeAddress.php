ef5a03a71956c24c50cfac37fc3218cb7f4ea471
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-31 10:48:16 -0600

    pushing staging to AWS through Github

---
+++ changeAddress.php
@@ -0,0 +1,38 @@
+<?php
+	include 'sessionStart.php'; 
+	include 'database.php';
+    $pdo = Database::connect();
+
+	//echo $_SESSION['user'];
+	if($_SERVER["REQUEST_METHOD"] == "POST")
+
+	$_POST['street1'];
+	$_POST['street2'];
+	$_POST['zipcode'];
+	$_POST['city'];
+	$_POST['state'];
+	$_POST['country'];
+
+	if(trim($firstName) != "" && trim($lastName) != "" && trim($username) != "" && trim($phoneNumber) != "" && trim($dob) != "" && trim($gender) != "" && trim($email) != "" && trim($password))
+	{
+		//echo "Got inside long if statement <br>";
+	    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql="INSERT INTO address (street_one, street_two, zipcode, city, state, country) VALUES (?, ?, ?, ?, ?, ?)";
+	    $q = $pdo->prepare($sql);
+	    $q->execute(array($_POST['street1'], $_POST['street2'], $_POST['zipcode'], $_POST['city'], $_POST['state'], $_POST['country']));
+
+	   	$pdo2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql2="INSERT INTO customer_address (customer_id, address_id) VALUES (?, ?)";
+	    $q2 = $pdo2->prepare($sql2);
+	    $q2->execute(array($_SESSION['customerid'], mysql_insert_id());
+	    
+    	header('Location: profile.php');
+	}else
+	{
+		$_SESSION['ErrorMessage'] =  "Fill in all required fields.";
+		header('Location: register.php');
+	}
+
+
+
+?>
