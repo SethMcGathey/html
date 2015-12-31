856c343a1e8f623775144c69827aec8f38fb0388
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-31 11:01:09 -0600

    pushing staging to AWS through Github

--- changeAddress.php
+++ changeAddress.php
@@ -23,7 +23,7 @@
 	    $q = $pdo->prepare($sql);
 	    $q->execute(array($_POST['street1'], $_POST['street2'], $_POST['zipcode'], $_POST['city'], $_POST['state'], $_POST['country']));
 	    //echo mysql_insert_id();
-	   	//$pdo2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	   	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$sql="INSERT INTO customer_address (customer_id, address_id) VALUES (?, ?)";
 	    $q = $pdo->prepare($sql);
 	    $q->execute(array($_SESSION['customerid'], mysql_insert_id());
