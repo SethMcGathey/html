d02b75321e2112ca84afef786ae811f50469ab9c
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2015-12-31 13:48:14 -0600

    pushing staging to AWS through Github

--- changeAddress.php
+++ changeAddress.php
@@ -15,33 +15,18 @@
 	    $q = $pdo->prepare($sql);
 	    $q->execute(array($_POST['street1'], $_POST['street2'], $_POST['zipcode'], $_POST['city'], $_POST['state'], $_POST['country']));
 
-	    echo $pid = $pdo->lastInsertID();
-	    /*$sql="SELECT ID FROM address ORDER BY ID desc LIMIT 5";
-	    $data = $pdo->query($sql);
+	    $pid = $pdo->lastInsertID();
 
-	    echo fetch_assoc($data);*/
-
-	    /*$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql="SELECT ID FROM address ORDER BY ID desc LIMIT 5";
-	    $q = $pdo->prepare($sql);
-	    $data = $q->execute(array($_SESSION['customerid'], 1));
-	    print_r($data);*/
-
-
-
-	    //echo $pdo::db2_last_insert_id($sql);
-	    //echo mysql_insert_id();
-	    //echo $_SESSION['customerid'];
 	   	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$sql="INSERT INTO customer_address (customer_id, address_id) VALUES (?, ?)";
 	    $q = $pdo->prepare($sql);
 	    $q->execute(array($_SESSION['customerid'], $pid));
 	    
-    	//header('Location: profile.php');
+    	header('Location: profile.php');
 	}else
 	{
 		$_SESSION['ErrorMessage'] =  "Fill in all required fields.";
-		//header('Location: register.php');
+		header('Location: register.php');
 	}
 
 
