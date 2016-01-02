897eec4d276c1b5178e26628b2b31b87b3736c7c
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-01 20:20:45 -0600

    pushing staging to AWS through Github

--- cart.php
+++ cart.php
@@ -9,7 +9,7 @@
 
 
 			<?php
-				$sql = "SELECT p.id, name, cost, p.description, SUM(quantity) as fullQuantity, image FROM transaction t JOIN transaction_product tp ON tp.transaction_id = t.id JOIN product p ON p.id = tp.product_id JOIN image i ON i.product_id = p.id WHERE cart = 1 AND customer_ID = 3 GROUP BY id";
+				$sql = 'SELECT p.id, name, cost, p.description, SUM(quantity) as fullQuantity, image FROM transaction t JOIN transaction_product tp ON tp.transaction_id = t.id JOIN product p ON p.id = tp.product_id JOIN image i ON i.product_id = p.id WHERE cart = 1 AND customer_ID = 3 GROUP BY id';
 						//$sql = 'SELECT id,name,cost,description FROM product WHERE subcategory_id = ' . $_GET["id"] . ' ORDER BY id LIMIT 5';
 						foreach ($pdo->query($sql) as $row) {
 						    echo '<div class="col-4-lg product" id="' . $row['p.id']. '">' . '<img src="data:image/jpeg;base64,' . base64_encode($row['image']) . '"width="100px"/> ' . $row['name'] . ' ' . $row['p.description'] . ' ' . $row['cost'] . ' ' . $row['fullQuantity'] . '</div>';
