55ea57adbc0e154dd8c0ba374347e9fdd62b056d
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-02 15:31:40 -0600

    pushing staging to AWS through Github

--- setAddress.php
+++ setAddress.php
@@ -3,15 +3,10 @@
 	include 'database.php';
     $pdo = Database::connect();
 
-	//echo $_SESSION['user'];
-	if($_SERVER["REQUEST_METHOD"] == "POST")
-		$junk = 'set junk';
-	
 	$_SESSION['addressIdForPurchase'] = $_GET['addressid'];
-	//echo $_GET['addressid'];
-	echo $_SESSION['addressIdForPurchase'];
-	echo $_GET['addressid'];
-	//header('Location: choosePurchasePayment.php');
+
+	header('Location: choosePurchasePayment.php');
+	
  	Database::disconnect();
 	
 ?>
--- setPaymentID.php
+++ setPaymentID.php
@@ -3,14 +3,9 @@
 	include 'database.php';
     $pdo = Database::connect();
 
-	//echo $_SESSION['user'];
-	if($_SERVER["REQUEST_METHOD"] == "POST")
-	$junk = 'set junk';
-
 	$_SESSION['paymentIdForPurchase'] = $_GET['paymentid'];
-	//echo $_GET['paymentid'];
-	echo $_SESSION['paymentIdForPurchase'];
-	//header('Location: confirmPurchase.php');
+
+	header('Location: confirmPurchase.php');
 
 	Database::disconnect();
 ?>
