d3c287f5394187bc5e67fca315cf99cfc141713e
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-02 15:34:26 -0600

    pushing staging to AWS through Github

--- confirmPurchase.php
+++ confirmPurchase.php
@@ -12,12 +12,10 @@
 						//echo $_SESSION['customerid'];
 		               	$sql = "SELECT card_full_name, card_number, card_security, expires_month, expires_year FROM payment WHERE id = " . $_SESSION['paymentIdForPurchase'];
 		               	foreach ($pdo->query($sql) as $row) {
-			               	echo '<a href="setPaymentID.php?paymentid=' . $row['id'] . '">
-			               		  <p name="nameOnCard" id="nameOnCard">Name on Card: ' . $row['card_full_name'] . '</p>
+			               	echo '<p name="nameOnCard" id="nameOnCard">Name on Card: ' . $row['card_full_name'] . '</p>
 								  <p name="cardNumber" id="cardNumber">Card Number: ' . $row['card_number'] . '</p>
 								  <p name="securityCode" id="securityCode">Security Code: ' . $row['card_security'] . '</p>
 								  <p name="expiration" id="expiration">Expires: ' . $row['expires_month'] . '/' . $row['expires_year'] . '</p>
-								  </a>
 								  <br>';
 		               }
 		            ?>
@@ -30,14 +28,12 @@
 						//echo $_SESSION['customerid'];
 		               	$sql = "SELECT street_one, street_two, zipcode, city, state, country FROM address WHERE id = " . $_SESSION['addressIdForPurchase'];
 		               	foreach ($pdo->query($sql) as $row) {
-			               	echo '<a href="setAddress.php?addressid=' . $row['id'] . '">
-			               		  <p name="street1" id="street1">Street 1: ' . $row['street_one'] . '</p>
+			               	echo '<p name="street1" id="street1">Street 1: ' . $row['street_one'] . '</p>
 								  <p name="street2" id="street2">Street 2: ' . $row['street_two'] . '</p>
 								  <p name="zipcode" id="zipcode">Zipcode: ' . $row['zipcode'] . '</p>
 								  <p name="city" id="city">City: ' . $row['city'] . '</p>
 								  <p name="state" id="state">State: ' . $row['state'] . '</p>
 								  <p name="country" id="country">Country: ' . $row['country'] . '</p>
-								  </a>
 								  <br>';
 		               }
 		            ?>
