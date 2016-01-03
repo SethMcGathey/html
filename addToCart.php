84f1585428e5bc0487004c2e435944f13aec78c4
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-02 18:43:20 -0600

    pushing staging to AWS through Github

--- addToCart.php
+++ addToCart.php
@@ -4,9 +4,14 @@
     $pdo = Database::connect();
 
 	//echo $_SESSION['user'];
-	if($_SERVER["REQUEST_METHOD"] == "POST")
+	//if($_SERVER["REQUEST_METHOD"] == "POST")
 
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   	$sql = "SELECT id, cart FROM transaction WHERE customer_id = " . $_SESSION['customerid'];
+   	$result = $pdo->query($sql);
+   	echo $result;
+
+
+	/*$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$sql="INSERT INTO transaction (cart, timestamp, payment_id, customer_id) VALUES (?, ?, ?, ?)";
     $q = $pdo->prepare($sql);
     $q->execute(array(1, time(), NULL, $_SESSION['customerid']));
@@ -14,9 +19,21 @@
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$sql="INSERT INTO transaction_product (quantity, transaction_id, product_id) VALUES (?, ?, ?)";
     $q = $pdo->prepare($sql);
-    $q->execute(array(1, $pdo->lastInsertID(), $_GET["id"]));
-
+    $q->execute(array(1, $pdo->lastInsertID(), $_GET["id"]));*/
 
 	Database::disconnect();
 
+	/*
+		SELECT id, cart, timestamp, payment_id, customer_id FROM transaction WHERE customer_id = 3;
+
+		SELECT * FROM transaction_product;
+
+		SELECT * FROM product;
+	*/
+
 ?>
+
+
+
+
+
