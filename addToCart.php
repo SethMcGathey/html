3e528429bff6e39fb53a0299f4b2f03fe22a79e9
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-03 10:21:33 -0600

    pushing staging to AWS through Github

--- addToCart.php
+++ addToCart.php
@@ -6,18 +6,19 @@
 	//echo $_SESSION['user'];
 	//if($_SERVER["REQUEST_METHOD"] == "POST")
 
-    $transactionid = 0;
+    $_SESSION['transaction_id'] = 0;
    	$sql = "SELECT id, cart FROM transaction WHERE customer_id = " . $_SESSION['customerid'] . ' AND cart = 1';
 
 	foreach ($pdo->query($sql) as $row) {
-		$transactionid = $row['id'];
+		$_SESSION['transaction_id'] = $row['id'];
+		//$transactionid = $row['id'];
 	}
 	Database::disconnect();
 
 	$pdo = Database::connect();
-	if(trim($transactionid) > 0)
+	if(trim($_SESSION['transaction_id']) > 0)
 	{
-		$sql = "SELECT id, quantity FROM transaction_product WHERE product_id = " . $_GET['id'] . ' AND transaction_id = ' . $transactionid;
+		$sql = "SELECT id, quantity FROM transaction_product WHERE product_id = " . $_GET['id'] . ' AND transaction_id = ' . $_SESSION['transaction_id'];
 
 		foreach ($pdo->query($sql) as $row) {
 			$row['id'];
@@ -26,26 +27,26 @@
 		if($quantity > 0)
 		{
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql="UPDATE transaction_product SET quantity = (?) WHERE transaction_id = " . $transactionid . " AND product_id = " . $_GET['id'];
+			$sql="UPDATE transaction_product SET quantity = (?) WHERE transaction_id = " . $_SESSION['transaction_id'] . " AND product_id = " . $_GET['id'];
 		    $q = $pdo->prepare($sql);
 		    $q->execute(array($quantity+1));
+		}else
+		{
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql="INSERT INTO transaction_product (quantity, transaction_id, product_id) VALUES (?, ?, ?)";
+		    $q = $pdo->prepare($sql);
+		    $q->execute(array(1, $_SESSION['transaction_id'], $_GET["id"]));
 		}
 	}else
 	{
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql="INSERT INTO transaction_product (quantity, transaction_id, product_id) VALUES (?, ?, ?)";
+		$sql="INSERT INTO transaction (cart, timestamp, payment_id, customer_id) VALUES (?, ?, ?, ?)";
 	    $q = $pdo->prepare($sql);
-	    $q->execute(array(1, $transactionid, $_GET["id"]));
+	    $q->execute(array(1, time(), NULL, $_SESSION['customerid']));
 	}
 
-/*
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql="INSERT INTO transaction (cart, timestamp, payment_id, customer_id) VALUES (?, ?, ?, ?)";
-    $q = $pdo->prepare($sql);
-    $q->execute(array(1, time(), NULL, $_SESSION['customerid']));
-*/
 
-
+	header('Location: products.php');
 	Database::disconnect();
 
 	/*
---
+++ updateCartAfterPurchase.php
@@ -0,0 +1,18 @@
+<?php
+	include 'sessionStart.php'; 
+	include 'database.php';
+    $pdo = Database::connect();
+
+	//$_SESSION['paymentIdForPurchase'] = $_GET['paymentid'];
+	//$_SESSION['addressIdForPurchase'] = $_GET['addressid'];
+	
+
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$sql="UPDATE transaction SET cart = 0 WHERE transaction_id = " . $_SESSION['transaction_id'];
+    $q = $pdo->prepare($sql);
+    $q->execute();
+
+    header('Location: confirmPurchase.php');
+
+	Database::disconnect();
+?>
