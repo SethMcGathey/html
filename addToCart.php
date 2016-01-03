b01355e345912807b3550d7e3b1423af04e06da3
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-02 22:53:18 -0600

    pushing staging to AWS through Github

--- addToCart.php
+++ addToCart.php
@@ -6,20 +6,42 @@
 	//echo $_SESSION['user'];
 	//if($_SERVER["REQUEST_METHOD"] == "POST")
 
-   	$sql = "SELECT id, cart FROM transaction WHERE customer_id = " . $_SESSION['customerid'];
-   	$result = $pdo->query($sql);
-   	echo $result;
+    $transactionid = 0;
+   	$sql = "SELECT id, cart FROM transaction WHERE customer_id = " . $_SESSION['customerid'] . ' AND cart = 1';
 
+	foreach ($pdo->query($sql) as $row) {
 
-	/*$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$transactionid = $row['id'];
+	}
+
+	if(trim($transactionid) > 0)
+	{
+		$innerSql = "SELECT id, quantity FROM transaction_product WHERE product_id = " . $_GET['id'] . ' AND transaction_id = ' . $transactionid;
+
+		foreach ($pdo->query($sql) as $row) {
+
+			$quantity = $row['quantity'];
+		}
+
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql="UPDATE transaction_product SET quantity = (?)";
+	    $q = $pdo->prepare($sql);
+	    $q->execute(array($quantity+1));
+	}else
+	{
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql="INSERT INTO transaction_product (quantity, transaction_id, product_id) VALUES (?, ?, ?)";
+	    $q = $pdo->prepare($sql);
+	    $q->execute(array(1, $pdo->lastInsertID(), $_GET["id"]));
+	}
+
+/*
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$sql="INSERT INTO transaction (cart, timestamp, payment_id, customer_id) VALUES (?, ?, ?, ?)";
     $q = $pdo->prepare($sql);
     $q->execute(array(1, time(), NULL, $_SESSION['customerid']));
+*/
 
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql="INSERT INTO transaction_product (quantity, transaction_id, product_id) VALUES (?, ?, ?)";
-    $q = $pdo->prepare($sql);
-    $q->execute(array(1, $pdo->lastInsertID(), $_GET["id"]));*/
 
 	Database::disconnect();
 
