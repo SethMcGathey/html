d9080937d7d05fb527f1d84e74d7dced7a5f70b0
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-03 10:29:59 -0600

    pushing staging to AWS through Github

--- updateCartAfterPurchase.php
+++ updateCartAfterPurchase.php
@@ -8,11 +8,12 @@
 	echo 'made it';
 
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql="UPDATE transaction SET cart = 0 WHERE transaction_id = ?";
+	$sql="UPDATE transaction SET cart = 0 WHERE transaction_id = (?)";
     $q = $pdo->prepare($sql);
     $q->execute(array($_SESSION['transaction_id']));
     echo 'made it 2';
     header('Location: confirmPurchase.php');
 
 	Database::disconnect();
+
 ?>
