75ef81464ecc2c99c140e62354c16af862143e39
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-03 15:58:53 -0600

    pushing staging to AWS through Github

--- removeFromCart.php
+++ removeFromCart.php
@@ -7,7 +7,7 @@
 	//$_SESSION['addressIdForPurchase'] = $_GET['addressid'];
 	echo 'made it';
 	echo $_SESSION['transaction_id'];
-
+	echo $_GET['productid'];
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$sql="DELETE transaction_product WHERE transaction_id = " . $_SESSION['transaction_id'] . " AND product_id = " . $_GET['productid'];
     $q = $pdo->prepare($sql);
