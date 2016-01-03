84a4085fa34de8822340c53f0206187d693e67eb
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-03 10:22:35 -0600

    pushing staging to AWS through Github

--- updateCartAfterPurchase.php
+++ updateCartAfterPurchase.php
@@ -5,7 +5,7 @@
 
 	//$_SESSION['paymentIdForPurchase'] = $_GET['paymentid'];
 	//$_SESSION['addressIdForPurchase'] = $_GET['addressid'];
-	
+	echo 'made it';
 
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$sql="UPDATE transaction SET cart = 0 WHERE transaction_id = " . $_SESSION['transaction_id'];
