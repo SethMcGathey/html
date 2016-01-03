19dfcf59207dcba86649a99e867790262a66c86d
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-03 10:52:17 -0600

    pushing staging to AWS through Github

--- confirmation.php
+++ confirmation.php
@@ -5,7 +5,7 @@
 		<?php require 'navigation.php' ?>
 		<div class="container" id="Not_Ajax_Output">
 			<div class="row">
-				<div class="col-lg-6">
+				<div class="col-lg-12">
 					<h3>Thank you!</h3>
 					<p>Thank you for your purchase! Your purchase will be processed shortly!</p>
 				</div>
