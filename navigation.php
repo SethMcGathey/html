cdbcd2dbf96a3d860ea9672f9f5df5b2c591abb6
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-03 16:50:16 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -10,29 +10,33 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="index.php">Home</a></li>
-        <li><a href="products.php">Products</a></li>
         <?php
+          $ifActive = array("index.php"=>"", "products.php"=>"", "sqlManagmentFiles/productFiles/productIndex.php"=>"", "contact.php"=>"", "cart.php"=>"", "logout.php"=>"", "profile.php"=>"", "register.php"=>"", "login.php"=>"");
+
+          echo '<li class="' . $ifActive[0] . '"><a href="index.php">Home</a></li>';
+          echo '<li class="' . $ifActive[1] . '"><a href="products.php">Products</a></li>';
+
           if($_SESSION['permission'] == 1)
           {
-            echo '<li><a href="sqlManagmentFiles/productFiles/productIndex.php">Admin</a></li>';
+            echo '<li class="' . $ifActive[2] . '"><a href="sqlManagmentFiles/productFiles/productIndex.php">Admin</a></li>';
           }
+        
+          //<li><a href="addProducts.php">Add Products</a></li>
+          echo '<li class="' . $ifActive[3] . '"><a href="contact.php">Contact</a></li>';
         ?>
-        <!--<li><a href="addProducts.php">Add Products</a></li>-->
-        <li><a href="contact.php">Contact</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>
-        <?php 
+        <?php
+          echo '<li class="' . $ifActive[4] . '"><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>';
               //changes login to logout when a person logs in and puts their name in the Navbar
               if(isset($_SESSION['username']))
               {
-                echo '<li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span>Logout</a></li>';
-                echo '<li><a href="profile.php">Welcome, ' . $_SESSION['username'] . '</a></li>';
+                echo '<li class="' . $ifActive[5] . '"><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span>Logout</a></li>';
+                echo '<li class="' . $ifActive[6] . '"><a href="profile.php">Welcome, ' . $_SESSION['username'] . '</a></li>';
               }else
               {
-                echo '<li><a href="register.php"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>';
-                echo '<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>';
+                echo '<li class="' . $ifActive[7] . '"><a href="register.php"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>';
+                echo '<li class="' . $ifActive[8] . '"><a href="login.php"><span class="glyphicon glyphicon-log-in"></span>Login</a></li>';
               }
         ?>
         
