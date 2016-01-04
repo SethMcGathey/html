8a01279fedf6d0d069ca458346654a1366b4d8d1
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-03 19:49:02 -0600

    pushing staging to AWS through Github

--- navigation.php
+++ navigation.php
@@ -11,6 +11,7 @@
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
         <?php
+          //sets up an associative array with the url of each page as the name so that I can set that link to "active"
           $ifActive = array("/index.php"=>"", "/products.php"=>"", "/sqlManagmentFiles/productFiles/productIndex.php"=>"", "/contact.php"=>"", "/cart.php"=>"", "/logout.php"=>"", "/profile.php"=>"", "/register.php"=>"", "/login.php"=>"");
 
           $ifActive[$_SERVER['PHP_SELF']] = "active";
@@ -22,13 +23,20 @@
             echo '<li class="' . $ifActive["/sqlManagmentFiles/productFiles/productIndex.php"] . '"><a href="sqlManagmentFiles/productFiles/productIndex.php">Admin</a></li>';
           }
         
-          //<li><a href="addProducts.php">Add Products</a></li>
           echo '<li class="' . $ifActive["/contact.php"] . '"><a href="contact.php">Contact</a></li>';
 
       echo '</ul>
       <ul class="nav navbar-nav navbar-right">';
 
-          echo '<li class="' . $ifActive["/cart.php"] . '"><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>';
+
+          $sql = 'SELECT SUM(quantity) as fullQuantity FROM transaction t JOIN transaction_product tp ON tp.transaction_id = t.id JOIN product p ON p.id = tp.product_id JOIN image i ON i.product_id = p.id WHERE cart = 1 AND customer_ID = 3';
+            //$sql = 'SELECT id,name,cost,description FROM product WHERE subcategory_id = ' . $_GET["id"] . ' ORDER BY id LIMIT 5';
+            foreach ($pdo->query($sql) as $row) {
+                $quantity = $row['fullQuantity'];
+            }
+
+
+          echo '<li class="' . $ifActive["/cart.php"] . '"><a href="cart.php">' . $quantity . '<span class="glyphicon glyphicon-shopping-cart"></span>Cart</a></li>';
               //changes login to logout when a person logs in and puts their name in the Navbar
               if(isset($_SESSION['username']))
               {
