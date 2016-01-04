3b881ba15aab5840d305c51cbb2597ecbe97fd3d
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-03 18:45:28 -0600

    pushing staging to AWS through Github

--- index.php
+++ index.php
@@ -7,14 +7,14 @@
 
 		<?php require 'navigation.php' ?> 
 		<div class="container-fluid" id="Not_Ajax_Output">
-			<h1>Index.php</h1>
+			<h1>Home Page</h1>
 			<div class="row">
 
               <?php
                $sql = 'SELECT id,name FROM category ORDER BY id';
                foreach ($pdo->query($sql) as $row) {
 	                echo '<a href="#">
-	                		<div class="col-lg-4 myCategories" id="' . $row['id']. '">
+	                		<div class="col-lg-4 myCategories mediumGrey" id="' . $row['id']. '">
 	                			<img src="img/rrwggame.jpg" width="100px"/>' . $row['name'] . '
 	                		</div>
 	                	  </a>';
@@ -22,14 +22,11 @@
               ?>
           	</div>
 
-
 			<div id="inner_ajax_Output">
 			</div>
-		
 		</div>
 
 		<div class="container-fluid" id="ajax_Output">
-			<div id='name'></div>
 		</div>
 
 
