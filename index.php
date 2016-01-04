800777d1f1f33a35fe71e3a35ab0d8dd9bc7cd2f
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-03 20:00:44 -0600

    pushing staging to AWS through Github

--- index.php
+++ index.php
@@ -27,6 +27,12 @@
 						$num = 0;
 	                }
                }
+
+               	$sql = 'SELECT id,name,category_id FROM subcategory WHERE category_id = ' . $_GET["id"];
+					foreach ($pdo->query($sql) as $row) {
+						//echo 'hello';
+				    	echo '<a href="products.php?id=' . $row['id']. '"><div class="col-4-lg" id="' . $row['id']. '">' . $row['name'] . '</div></a>';
+					}
               ?>
           	</div>
 
