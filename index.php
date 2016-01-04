09350b3b271b64198fdeb1a787d0d58b89bb33cb
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-03 20:15:51 -0600

    pushing staging to AWS through Github

--- index.php
+++ index.php
@@ -35,9 +35,18 @@
 			<div id="inner_ajax_Output">
 				<?php
 				    $sql = 'SELECT id,name,category_id FROM subcategory WHERE category_id = 1';
+				    $num = 0;
 					foreach ($pdo->query($sql) as $row) {
 						//echo 'hello';
-				    	echo '<a href="products.php?id=' . $row['id']. '"><div class="col-4-lg" id="' . $row['id']. '">' . $row['name'] . '</div></a>';
+				    	echo '<a href="products.php?id=' . $row['id']. '"><div class="col-4-lg leftRight' . $num . '" id="' . $row['id']. '">' . $row['name'] . '</div></a>';
+
+				    	if($num < 1){
+				    		$num++;
+				    	}else
+				    	{
+				    		$num = 0;
+				    	}
+				    	
 					}
 				?>
 			</div>
