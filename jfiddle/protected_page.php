0f009d57343018c03fa395faa140e4263c73a896
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-01-30 15:37:49 -0600

    pushing staging to AWS through Github

--- jfiddle/protected_page.php
+++ jfiddle/protected_page.php
@@ -8,28 +8,23 @@ sec_session_start();
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Session Scheduler</title>
-        <link rel="stylesheet" href="css/bootstrap.css" />
-        <link rel="stylesheet" href="css/fullcalendar.css">
-        <link rel="stylesheet" href="css/styles.css" />
+        <title>Secure Login: Protected Page</title>
+        <link rel="stylesheet" href="styles/main.css" />
     </head>
     <body>
         <?php if (login_check($mysqli) == true) : ?>
             <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>
-                <div ui-calendar ng-model="eventSources"></div>
+            <p>
+                This is an example protected page.  To access this page, users
+                must be logged in.  At some stage, we'll also check the role of
+                the user, so pages will be able to determine the type of user
+                authorised to access the page.
+            </p>
             <p>Return to <a href="index.php">login page</a></p>
         <?php else : ?>
             <p>
                 <span class="error">You are not authorized to access this page.</span> Please <a href="index.php">login</a>.
             </p>
         <?php endif; ?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="js/moment.min.js"></script>
-    <script src="js/angular.js"></script>
-    <script src="js/calendar.js"></script>
-    <script src="js/fullcalendar.js"></script>
-    <script src="js/gcal.js"></script>
-    <script src="js/controllers.js"></script>
-    <script src="https://apis.google.com/js/client.js"></script>
     </body>
 </html>
