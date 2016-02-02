5efdf991c33d5de39fbf8c9b0bb8571b844ec8e3
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-01 21:18:33 -0600

    pushing staging to AWS through Github

--- jfiddle/overwriteFile.php
+++ jfiddle/overwriteFile.php
@@ -1,8 +1,8 @@
 <?php
 	//$myfile = fopen("test.txt","w");
 //error_checking(E_ALL);
-var_dump(ini_get('allow_url_fopen'));
-	error_reporting(E_ALL);
+	//var_dump(ini_get('allow_url_fopen'));
+	//error_reporting(E_ALL);
 	$myfile = fopen("temp.txt", "w") or die("Unable to open file!");
 	$txt = "Mickey Mouse\n";php 
 	fwrite($myfile, $txt);
