cca0013dadec87b144b7a61ce56b64baad6a5b6d
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-02 18:26:21 -0600

    pushing staging to AWS through Github

--- jfiddle/js/javascript.js
+++ jfiddle/js/javascript.js
@@ -1,23 +1,38 @@
 
-var content = "";
-var iframe = document.getElementById('iframe');
+$(document).ready(function(){
+	var content = "";
+	var iframe = document.getElementById('iframe');
 
-if(iframe.contentDocument)
-{
-	doc = iframe.contentDocument;
-}
-else if(iframe.contentWindow)
-{
-	doc = iframe.contentWindow.document;
-}else 
-{
-	doc = iframe.document;
-}
+	if(iframe.contentDocument)
+	{
+		doc = iframe.contentDocument;
+	}
+	else if(iframe.contentWindow)
+	{
+		doc = iframe.contentWindow.document;
+	}else 
+	{
+		doc = iframe.document;
+	}
 
-doc.open();
-doc.writeln(result);
-doc.close();
+	doc.open();
+	doc.writeln(result);
+	doc.close();
+});
 
+
+
+function passStrings(){
+
+		var htmlString = $('#htmlDiv').html(); 
+		var javascriptString = $('#javascriptDiv').html(); 
+		var cssString = $('#cssDiv').html(); 
+
+		console.log(htmlString);
+		console.log(javascriptString);
+		console.log(cssString);
+		//window.location = "overwriteFile.php?htmlString=" + htmlString + "javascriptString=" + javascriptString + "cssString=" + cssString;
+});
 
 
 
