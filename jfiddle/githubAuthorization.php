d4f60d19882fbed54526f3e7664f77fb179bf42a
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-03 19:08:44 -0600

    pushing staging to AWS through Github

--- jfiddle/githubAuthorization.php
+++ jfiddle/githubAuthorization.php
@@ -6,7 +6,7 @@ $provider = new League\OAuth2\Client\Provider\Github([
     'redirectUri'       => 'http://ec2-52-34-213-191.us-west-2.compute.amazonaws.com/jfiddle/codingPage.php',
 ]);
 //echo "Made it here";
-if (!isset($_GET['code'])) {
+/*if (!isset($_GET['code'])) {
 
     // If we don't have an authorization code then get one
     $authUrl = $provider->getAuthorizationUrl();
@@ -47,4 +47,4 @@ if (!isset($_GET['code'])) {
 
     // Use this to interact with an API on the users behalf
     echo $token->getToken();
-}
+}*/
