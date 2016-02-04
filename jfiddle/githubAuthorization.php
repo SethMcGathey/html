939f74bfa5ecf98b95804375f273e5177b7ed288
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-03 21:24:58 -0600

    pushing staging to AWS through Github

--- jfiddle/githubAuthorization.php
+++ jfiddle/githubAuthorization.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL);
-$provider = new League\OAuth2\Client\Provider\Github([
+$provider = new Smcgath\OAuth2\Client\Provider\Github([
     'clientId'          => '{95a3734f1cec64f13af8}',
     'clientSecret'      => '{fa1f6166752cdf6ef743a09846303b4cc4b79747}',
     'redirectUri'       => 'http://ec2-52-34-213-191.us-west-2.compute.amazonaws.com/jfiddle/codingPage.php',
