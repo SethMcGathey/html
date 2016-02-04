5a37268f2d53ca92f82fd1b898b9daf54d5f16ef
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-03 21:12:28 -0600

    pushing staging to AWS through Github

--- jfiddle/githubAuthorization.php
+++ jfiddle/githubAuthorization.php
@@ -1,27 +1,23 @@
 <?php
-//echo "garbage";
-error_reporting(E_ALL);
+
 $provider = new League\OAuth2\Client\Provider\Github([
     'clientId'          => '{95a3734f1cec64f13af8}',
     'clientSecret'      => '{fa1f6166752cdf6ef743a09846303b4cc4b79747}',
     'redirectUri'       => 'http://ec2-52-34-213-191.us-west-2.compute.amazonaws.com/jfiddle/codingPage.php',
 ]);
-//echo "garbage2";
-//echo "Made it here";
+
 if (!isset($_GET['code'])) {
 
     // If we don't have an authorization code then get one
     $authUrl = $provider->getAuthorizationUrl();
     $_SESSION['oauth2state'] = $provider->getState();
     header('Location: '.$authUrl);
-    //echo "oauth2state " . $_SESSION['oauth2state'];
     exit;
 
 // Check given state against previously stored one to mitigate CSRF attack
 } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
 
     unset($_SESSION['oauth2state']);
-    //echo "oauth2state after unset" . $_SESSION['oauth2state'];
     exit('Invalid state');
 
 } else {
@@ -29,7 +25,6 @@ if (!isset($_GET['code'])) {
     // Try to get an access token (using the authorization code grant)
     $token = $provider->getAccessToken('authorization_code', [
         'code' => $_GET['code']
-        //echo "code " . $_GET['code'];
     ]);
 
     // Optional: Now you have a token you can look up a users profile data
