c7c5e90c8bf8497f1a8f7e2c58c775f2341e4c53
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-04 21:15:42 -0600

    pushing staging to AWS through Github

--- jfiddle/githubReadRepo.php
+++ jfiddle/githubReadRepo.php
@@ -1,6 +1,6 @@
 <?php
-
-var repo = github.getRepo($_POST['username'], $_POST['reponame']);
+var repo = github.getRepo('Smcgath', 'Smcgath.github.io');
+//var repo = github.getRepo($_POST['username'], $_POST['reponame']);
 /*repo.read('master', 'index.html', function(err, data) {
 	//print(data);
 });*/
