621f4ba3ad40df7ea02cc2ba579d890be5c4dff7
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-06 12:01:29 -0600

    pushing staging to AWS through Github

--- jfiddle/js/javascript.js
+++ jfiddle/js/javascript.js
@@ -62,8 +62,8 @@ function passStrings(){
 function getGithubInstance(){
 
 	var github = new Github({
-	  username: "Smcgath",
-	  password: "1qaz2wsxQ",
+	  username: "YOU_USER",
+	  password: "YOUR_PASSWORD",
 	  auth: "basic"
 	  });
 
