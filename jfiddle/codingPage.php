7d29e273a056622db071c4f14730f67c16f5726e
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-06 11:57:15 -0600

    pushing staging to AWS through Github

--- jfiddle/codingPage.php
+++ jfiddle/codingPage.php
@@ -77,6 +77,7 @@
 <script src="js/jquery-2.1.4.min.js" type="text/javascript"> </script>
 <script src="js/bootstrap.min.js" type="text/javascript"></script>
 <script src="js/javascript.js" type="text/javascript"></script>
+<script src="js/github.js" type="text/javascript"></script>
 	
 </html>
 
