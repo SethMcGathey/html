e661a91955441a2d27008a515d37376630f56309
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-08 17:56:55 -0600

    pushing staging to AWS through Github

--- jfiddle/saveScripts.php
+++ jfiddle/saveScripts.php
@@ -15,15 +15,15 @@ ini_set('display_errors', 'on');
 	    $q->execute($_POST['html'], $_POST['javascript'], $_POST['css']);
 	}else
 	{*/
-		/*$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$sql="INSERT INTO codeStrings (html, javascript, css, memberId) VALUES (?, ?, ?, ?)";
 	    $q = $pdo->prepare($sql);
-	    $q->execute($_POST['html'], $_POST['javascript'], $_POST['css'], $_SESSION['memberId'];*/
+	    $q->execute($_POST['html'], $_POST['javascript'], $_POST['css'], $_SESSION['memberId'];
 	//}
 
 
 
-	header('Location: products.php');
+	//header('Location: products.php');
 	Database::disconnect();
 
 	/*
