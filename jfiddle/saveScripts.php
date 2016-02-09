6834de7ae71ec73e2ac7d6d902b3b2f8fb7ae1e9
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-08 19:40:03 -0600

    pushing staging to AWS through Github

--- jfiddle/saveScripts.php
+++ jfiddle/saveScripts.php
@@ -16,7 +16,7 @@ ini_set('display_errors', 'on');
 	}else
 	{*/
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql="INSERT INTO codeStrings (html, javascript, css, memberId) VALUES (" . $GET['html'] . ", " . $GET['javascript'] . ", " . $GET['css'] . ", " . $_SESSION['memberId'] . ")";
+		$sql="INSERT INTO codeStrings (html, javascript, css, memberId) VALUES (" . $_GET['html'] . ", " . $_GET['javascript'] . ", " . $_GET['css'] . ", " . $_SESSION['memberId'] . ")";
 	    $q = $pdo->prepare($sql);
 	    $q->execute();
 	//}
