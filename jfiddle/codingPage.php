c020e5625edd0504d1058748438fd62064f5e45a
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-11 23:01:08 -0600

    pushing staging to AWS through Github

--- jfiddle/codingPage.php
+++ jfiddle/codingPage.php
@@ -89,7 +89,7 @@
 <script src="js/jquery-1.12.0.js" type="text/javascript"> </script>
 
 <script src="codemirror/lib/codemirror.js"></script>
-<script src="codemirror/mode/htmlmixed/htmlmixed.js"></script>
+<script src="codemirror/mode/htmlembedded/htmlembedded.js"></script>
 <script src="codemirror/mode/css/css.js"></script>
 <script src="codemirror/mode/javascript/javascript.js"></script>
 
