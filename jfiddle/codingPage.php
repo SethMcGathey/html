7d86e8f1640e21934498975e42916413c5100fe7
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-11 21:03:36 -0600

    pushing staging to AWS through Github

--- jfiddle/codingPage.php
+++ jfiddle/codingPage.php
@@ -45,14 +45,14 @@
 						</div>
 						
 
-						<pre class="prettyprint col-lg-6 squareDivs" id="htmlDiv"  contenteditable>
+						<textarea class="prettyprint col-lg-6 squareDivs" id="htmlDiv"  contenteditable>
 window.onload = function() {
     var aCodes = document.getElementsByTagName('pre');
     for (var i=0; i < aCodes.length; i++) {
         hljs.highlightBlock(aCodes[i]);
     }
 };
-						</pre>
+						</textarea>
 
 						
 						<div class="col-lg-6 squareDivs prettyprint" id="cssDiv"  contenteditable>
