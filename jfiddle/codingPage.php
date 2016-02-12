5ec0bbb8367c8855bab01e833c02e714eb34dd4f
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-11 20:33:27 -0600

    pushing staging to AWS through Github

--- jfiddle/codingPage.php
+++ jfiddle/codingPage.php
@@ -88,11 +88,14 @@ window.onload = function() {
 ?>
 
 
-<script src="codemirror/lib/codemirror.js"></script>
-<script src="codemirror/mode/javascript/javascript.js"></script>
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="js/jquery-1.12.0.js" type="text/javascript"> </script>
+
+<script src="codemirror/lib/codemirror.js"></script>
+<script src="codemirror/mode/javascript/javascript.js"></script>
+
 <!--<script src="js/jquery-2.1.4.min.js" type="text/javascript"> </script>-->
 <script src="js/bootstrap.min.js" type="text/javascript"></script>
 <script src="js/github.js" type="text/javascript" ></script>
