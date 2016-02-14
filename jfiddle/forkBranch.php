b8f5b3e9efb22efe0d5d6fdd80656afe6eb52e05
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-14 17:32:21 -0600

    pushing staging to AWS through Github

--- jfiddle/forkBranch.php
+++ jfiddle/forkBranch.php
@@ -8,12 +8,12 @@
 		foreach ($pdo->query($sql) as $row) {
 			$topBranchId =  $row['branchId'] 
 		}
-
+/*
 		$pdo->setAttribute(PDO::ATTR_FETCH_TABLE_NAMES, true);
     	$sql="INSERT INTO codeStrings (html, javascript, css, projectId, branchId, commitId) VALUES (?, ?, ?, ?, ?, ?)";
 	    $q = $pdo->prepare($sql);
 	    $q->execute(array($_POST['html'], $_POST['javascript'], $_POST['css'], $_SESSION['productId'], $_SESSION['branchId'] + 1, 1));
-
+*/
 	Database::disconnect();
 ?>
 <!--
