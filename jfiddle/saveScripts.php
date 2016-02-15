3af881ee33bf43e654fb8c5b397a4b0a38542649
unknown <sethm@mil-sethm-dt7.marketprobe.priv>
2016-02-15 09:50:54 -0600

    trying old code

--- jfiddle/saveScripts.php
+++ jfiddle/saveScripts.php
@@ -1,5 +1,5 @@
-<?php
-error_reporting(E_ALL);
+<!--<?php
+/*error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 	require_once 'sessionStart.php'; 
 	require_once 'database.php';
@@ -35,7 +35,7 @@ ini_set('display_errors', 'on');
 		}
 */
 
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+/*		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		//$sql="INSERT INTO codeStrings (html, javascript, css, memberId) VALUES (" . $_GET['html'] . ", " . $_GET['javascript'] . ", " . $_GET['css'] . ", " . $_SESSION['memberId'] . ")";
 	    $sql="INSERT INTO codeStrings (html, javascript, css, projectId, branchId, commitId) VALUES (?, ?, ?, ?, ?, ?)";
 	    $q = $pdo->prepare($sql);
@@ -55,9 +55,45 @@ ini_set('display_errors', 'on');
 		SELECT * FROM product;
 	*/
 
+?>-->
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+	require_once 'sessionStart.php'; 
+	require_once 'database.php';
+    $pdo = Database::connect();
+
+    //$_SESSION["memberId"] = 1;
+	/*if($_SESSION['memberID'])
+	{
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql="UPDATE codeStrings SET html = (?), javascript = (?), css = (?) WHERE  memberId = " . $_SESSION['memberID'];
+	    $q = $pdo->prepare($sql);
+	    $q->execute($_POST['html'], $_POST['javascript'], $_POST['css']);
+	}else
+	{*/
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		//$sql="INSERT INTO codeStrings (html, javascript, css, memberId) VALUES (" . $_GET['html'] . ", " . $_GET['javascript'] . ", " . $_GET['css'] . ", " . $_SESSION['memberId'] . ")";
+	    $sql="INSERT INTO codeStrings (html, javascript, css, projectId, branchId, commitId) VALUES (?, ?, ?, ?, ?, ?)";
+	    $q = $pdo->prepare($sql);
+	    $q->execute(array($_POST['html'], $_POST['javascript'], $_POST['css'], $_SESSION['productId'], $_SESSION['branchId'], $_SESSION['commitId']));
+	//}
+
+
+
+	//header('Location: products.php');
+	Database::disconnect();
+
+	/*
+		SELECT id, cart, timestamp, payment_id, customer_id FROM transaction WHERE customer_id = 3;
+
+		SELECT * FROM transaction_product;
+
+		SELECT * FROM product;
+	*/
+
 ?>
-
-
 
 
 
