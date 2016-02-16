0c99c1a1204a60eacd22a712c009476ea66fa446
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-02-15 19:50:02 -0600

    stuff

--- jfiddle/zipFiles.php
+++ jfiddle/zipFiles.php
@@ -12,23 +12,23 @@ ini_set('display_errors', 'on');
 
 	$javascriptFile = fopen("jFiddleJavascript.js", "w") or die("Unable to open file!");
 	//$javascript = $_POST['javascriptString'];
-	$javascript = $_GET['javascriptString'];
-	$_SESSION['javascriptCode'] = $javascript;
-	//$javascript = 'junk';
+	//$javascript = $_GET['javascriptString'];
+	//$_SESSION['javascriptCode'] = $javascript;
+	$javascript = 'junk';
 	fwrite($javascriptFile, $javascript);
 	fclose($javascriptFile);
 
 	$htmlFile = fopen("jFiddleHtml.html", "w") or die("Unable to open file!");
 	//$html = $_POST['htmlString'];
-	$html = $_GET['htmlString'];
-	//$html = 'htmljunk';
+	//$html = $_GET['htmlString'];
+	$html = 'htmljunk';
 	fwrite($htmlFile, $html);
 	fclose($htmlFile);
 
 	$cssFile = fopen("jFiddleCss.css", "w") or die("Unable to open file!");
 	//$css = $_POST['cssString'];
-	$css = $_GET['cssString'];
-	//$css = 'cssjunk';
+	//$css = $_GET['cssString'];
+	$css = 'cssjunk';
 	fwrite($cssFile, $css);
 	fclose($cssFile);
 	
