6f83ce49773917e065fd80a291e7089d0a064414
Seth McGathey <Seth.McGathey@mail.wlc.edu>
2016-03-07 16:58:07 -0600

    pushing staging to AWS through Github

--- jfiddle/js/javascript.js
+++ jfiddle/js/javascript.js
@@ -7,7 +7,7 @@ $(function(){
 
 
 var htmlEditor = ace.edit("htmlDiv");
-htmlEditor.setTheme("ace/theme/iplastic");
+htmlEditor.setTheme("ace/theme/kuroir");
 htmlEditor.getSession().setMode("ace/mode/javascript");
 htmlEditor.$blockScrolling = 'Infinity';
 
@@ -20,7 +20,7 @@ htmlEditor.getSession().on('change', function(){
 
 
 var javascriptEditor = ace.edit("javascriptDiv");
-javascriptEditor.setTheme("ace/theme/katzenmilch");
+javascriptEditor.setTheme("ace/theme/merbivore_soft");
 javascriptEditor.getSession().setMode("ace/mode/javascript");
 javascriptEditor.$blockScrolling = 'Infinity';
 
@@ -33,7 +33,7 @@ javascriptEditor.getSession().on('change', function(){
 
 
 var cssEditor = ace.edit("cssDiv");
-cssEditor.setTheme("ace/theme/kr_theme");
+cssEditor.setTheme("ace/theme/merbivore");
 cssEditor.getSession().setMode("ace/mode/javascript");
 cssEditor.$blockScrolling = 'Infinity';
 
